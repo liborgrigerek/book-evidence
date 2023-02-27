1d05aac3f9a55e55c707afad8c67a499ecd0d362
Libor Grigerek <libor.grigerek@deutsche-boerse.com>
2023-02-27 13:40:58 +0100

    BE: init commit

---
+++ BE/book-evidence/src/main/java/com/example/java/bookevidence/BookEvidenceApplication.java
@@ -0,0 +1,23 @@
+package com.example.java.bookevidence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * This is an example of application that demonstrates using of:
+ * - BE: SpringBoot, JPA, Hibernate, H2
+ * - FE: TypeScript (Angular), Angular-Material-Design, JavaScript, CSS
+ * - OAuth2 authentication / authorization
+ * - Maven, Git / GitHub
+ * 
+ * @author Libor Grigerek
+ */
+
+@SpringBootApplication
+public class BookEvidenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookEvidenceApplication.class, args);
+	}
+
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Book Evidence Application
+
+This is an example of application that demonstrates using of:
+
+ - BE: SpringBoot, JPA, Hibernate, H2
+ - FE: TypeScript (Angular), Angular-Material-Design, JavaScript, CSS
+ - OAuth2 authentication / authorization
+ - Maven, Git / GitHub
+
