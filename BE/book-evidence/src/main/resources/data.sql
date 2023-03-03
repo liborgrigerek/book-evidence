cf28746d27ec378091fe7ca2c928fae89d7f2009
Libor Grigerek <libor.grigerek@deutsche-boerse.com>
2023-03-03 08:45:28 +0100

    BE: added init SQL script

--- BE/book-evidence/src/main/resources/data.sql
+++ BE/book-evidence/src/main/resources/data.sql
@@ -1,1 +1,5 @@
 -- init file for H2 database
+
+-- AUTHOR table
+INSERT INTO AUTHOR (FIRSTNAME, LASTNAME) VALUES ('Pepa', 'Kolotoc');
+INSERT INTO AUTHOR (FIRSTNAME, LASTNAME) VALUES ('Franta', 'Vokurka');
